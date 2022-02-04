a6234a33d6961051f29e58988ce6a5b997f7c3ed
Iva Petkova <iva1petkova@gmail.com>
2022-02-04 11:21:51 +0200

    added exam from 31/03/2020

--- MySQL/MYSQL_EXAM_PREP/src/main/java/20200331/20200331_exam.sql
+++ MySQL/MYSQL_EXAM_PREP/src/main/java/20200331/20200331_exam.sql
@@ -93,4 +93,74 @@ from addresses
 where id % 3 = 0;
 
 # 5. users
+select username, gender, age
+from users
+order by age desc, username;
 
+# 06.	Extract 5 Most Commented Photos
+select p.id,
+       p.date                                                date_and_time,
+       p.description,
+       (select count(*) from comments where photo_id = p.id) commentsCount
+from photos p
+order by commentsCount desc, id
+limit 5;
+
+# 07.	Lucky Users
+select concat_ws(' ', u.id, u.username) id_username, u.email
+from users u
+         join users_photos up on u.id = up.user_id
+where up.user_id = up.photo_id
+order by u.id;
+
+# 08.	Count Likes and Comments
+select p.id as photo_id,
+       (
+           select count(photo_id)
+           from likes
+           where p.id = photo_id
+       )       likes_count,
+       (
+           select count(id)
+           from comments
+           where photo_id = p.id
+       )       comments_count
+from photos p
+order by likes_count desc, comments_count desc, p.id desc;
+
+# 09.	The Photo on the Tenth Day of the Month
+select concat(left(p.description, 30), '...') summary,
+       p.date
+from photos p
+where day(p.date) = 10
+order by p.date desc;
+
+# 10.	Get User’s Photos Count
+create function udf_users_photos_count(username VARCHAR(30))
+    returns int
+    deterministic
+begin
+    return (
+        select count(up.photo_id)
+        from users u
+                 join users_photos up on u.id = up.user_id
+        where u.username = username
+    );
+end;
+
+SELECT udf_users_photos_count('ssantryd') AS photosCount;
+# 2
+
+# 11.	Increase User Age
+create procedure udp_modify_user(user_address varchar(30), user_town varchar(30))
+    deterministic
+begin
+    update users u join addresses a on u.id = a.user_id
+    set u.age = u.age + 10
+    where a.address = user_address
+      and a.town = user_town;
+end;
+
+CALL udp_modify_user ('97 Valley Edge Parkway', 'DivinГіpolis');
+SELECT u.username, u.email,u.gender,u.age,u.job_title FROM users AS u
+WHERE u.username = 'eblagden21';
