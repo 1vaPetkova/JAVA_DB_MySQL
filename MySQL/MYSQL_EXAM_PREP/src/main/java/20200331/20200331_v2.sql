1d715975b6003816c18cf7526b7d992104f3f59f
Iva Petkova <iva1petkova@gmail.com>
2022-02-11 13:04:11 +0200

    retake exam 31/03/2020

--- MySQL/MYSQL_EXAM_PREP/src/main/java/20200331/20200331_v2.sql
+++ MySQL/MYSQL_EXAM_PREP/src/main/java/20200331/20200331_v2.sql
@@ -98,4 +98,79 @@ select username, gender, age
 from users
 order by age desc, username;
 
-6.
+# 06. Extract 5 most commented photos
+select p.id,
+       p.date           date_and_time,
+       p.description,
+       count(c.comment) commentsCount
+from photos p
+         join comments c on p.id = c.photo_id
+group by p.id
+order by commentsCount desc, p.id
+limit 5;
+
+# 7. lucky users
+select concat(u.id, ' ', u.username) id_username,
+       u.email
+from users u
+         join users_photos up on u.id = up.user_id
+where up.user_id = up.photo_id
+order by u.id;
+
+# 08.	Count Likes and Comments
+select p.id as                                                photo_id,
+       (select count(id) from likes where photo_id = p.id)    likes_count,
+       (select count(id) from comments where photo_id = p.id) comments_count
+from photos p
+group by p.id
+order by likes_count desc, comments_count desc, p.id;
+
+select p.id as              photo_id,
+       count(distinct l.id) likes_count,
+       count(distinct c.id) comments_count
+from photos p
+         left join comments c on p.id = c.photo_id
+         left join likes l on p.id = l.photo_id
+group by p.id
+order by likes_count desc, comments_count desc, p.id;
+
+# 09.	The Photo on the Tenth Day of the Month
+select concat(left(p.description, 30), '...') summary, p.date
+from photos p
+where day(p.date) = 10
+order by date desc;
+
+# 10.	Get User’s Photos Count
+create function udf_users_photos_count(username VARCHAR(30))
+    returns int
+    deterministic
+begin
+    return (select count(up.photo_id)
+            from users_photos up
+                     join users u on up.user_id = u.id
+            where u.username = username
+    );
+end;
+
+# Query
+SELECT udf_users_photos_count('ssantryd') AS photosCount;
+# photosCount
+# 2
+
+# 11.	Increase User Age
+create procedure udp_modify_user(user_address VARCHAR(30), user_town VARCHAR(30))
+    deterministic
+begin
+    update users u join addresses a on u.id = a.user_id
+    set age = age + 10
+    where a.town = user_town and a.address = user_address;
+end;
+
+CALL udp_modify_user ('97 Valley Edge Parkway', 'DivinГіpolis');
+SELECT u.username, u.email,u.gender,u.age,u.job_title FROM users AS u
+WHERE u.username = 'eblagden21';
+
+
+update users u join addresses a on u.id = a.user_id
+set age = age + 10
+where a.town = 'DivinГіpolis' and a.address ='97 Valley Edge Parkway';
